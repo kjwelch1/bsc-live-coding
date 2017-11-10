acb5d51501eef7be1d24bf4237a757179f77c3e7
Ed Powley <edpowley@gmail.com>
2017-11-10 10:59:24 +0000

    Forgot to commit evolutionary algorithm implementation from COMP250

--- COMP250/08_evolution/main.py
+++ COMP250/08_evolution/main.py
@@ -9,6 +9,11 @@ TILE_SIZE = 25
 MAP_WIDTH = 20
 MAP_HEIGHT = 20
 
+POPULATION_SIZE = 20
+TOURNAMENT_SIZE = 5
+ELITISM = POPULATION_SIZE / 10
+MUTATION = 0.5
+
 
 def wait_key():
     while True:
@@ -44,13 +49,35 @@ def calculate_fitness(the_map):
     return pathfinding.get_path_length(path)
 
 
-def main():
-    tiles = [' '] * (MAP_WIDTH * MAP_HEIGHT)
-    tiles[0] = 'S'
-    tiles[-1] = 'G'
+def tournament_select(population):
+    tournament = []
+    for i in xrange(TOURNAMENT_SIZE):
+        tournament.append(random.choice(population))
 
-    the_map = map.Map((MAP_WIDTH, MAP_HEIGHT), tiles, TILE_SIZE, include_diagonals=True)
-    current_fitness = calculate_fitness(the_map)
+    population.sort()
+    return population[-1][1]
+
+
+def main():
+    # Initial population
+    population = []
+    for i in xrange(POPULATION_SIZE):
+        tiles = [' '] * (MAP_WIDTH * MAP_HEIGHT)
+        tiles[0] = 'S'
+        tiles[-1] = 'G'
+
+        # Randomly set 10% of tiles
+        for j in xrange(len(tiles) / 10):
+            r = random.randrange(len(tiles))
+            if tiles[r] == ' ':
+                tiles[r] = '*'
+
+        the_map = map.Map((MAP_WIDTH, MAP_HEIGHT), tiles, TILE_SIZE, include_diagonals=True)
+        fitness = calculate_fitness(the_map)
+
+        population.append((fitness, tiles))
+
+    population.sort()
 
     # Initialise PyGame
     pygame.init()
@@ -63,29 +90,41 @@ def main():
     # Create the screen
     screen = pygame.display.set_mode(window_size)
 
-    index = 0
-
     while True:
-        index += 1
-        if index >= len(tiles):
-            index = 0
+        new_population = [x[1] for x in population[-ELITISM:]]
 
+        while len(new_population) < POPULATION_SIZE:
+            if random.uniform(0, 1) < MUTATION:
+                mutated = tournament_select(population)[:]
+                for i in xrange(len(mutated) / 20):
+                    r = random.randrange(len(mutated))
+                    if mutated[r] == ' ':
+                        mutated[r] = '*'
+                    elif mutated[r] == '*':
+                        mutated[r] = ' '
+
+                new_population.append(mutated)
+            else:
+                parent_a = tournament_select(population)[:]
+                parent_b = tournament_select(population)[:]
+                crossover_point = random.randrange(len(parent_a))
+                child = parent_a[:crossover_point] + parent_b[crossover_point:]
+
+                new_population.append(child)
+
+        population = []
+        for tiles in new_population:
+            the_map = map.Map((MAP_WIDTH, MAP_HEIGHT), tiles, TILE_SIZE, include_diagonals=True)
+            fitness = calculate_fitness(the_map)
+
+            population.append((fitness, tiles))
+
+        population.sort()
+        fitness, tiles = population[-1]
         the_map = map.Map((MAP_WIDTH, MAP_HEIGHT), tiles, TILE_SIZE, include_diagonals=True)
-        print "Current fitness:", current_fitness
+        print "Current fitness:", fitness
         path = get_path(the_map)
         draw_map_and_path(screen, the_map, path)
-
-        new_tiles = tiles[:]
-        #index = random.randrange(len(new_tiles))
-        if new_tiles[index] == ' ':
-            new_tiles[index] = '*'
-
-            new_map = map.Map((MAP_WIDTH, MAP_HEIGHT), new_tiles, TILE_SIZE, include_diagonals=True)
-            new_fitness = calculate_fitness(new_map)
-
-            if new_fitness > current_fitness:
-                tiles = new_tiles
-                current_fitness = new_fitness
 
         # wait_key()
 
